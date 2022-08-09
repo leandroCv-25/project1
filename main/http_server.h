d4e96a4491200b4a4eeade82c9a2acdfafed1621
leandroCv-25 <leandrocamposvargas@gmail.com>
2022-08-09 20:04:23 -0300

    OTA

--- main/http_server.h
+++ main/http_server.h
@@ -1,24 +1,9 @@
-/*
- * http_server.h
- *
- *  Created on: Oct 20, 2021
- *      Author: kjagu
- */
-
 #ifndef MAIN_HTTP_SERVER_H_
 #define MAIN_HTTP_SERVER_H_
 
-/**
- * Connection status for Wifi
- */
-typedef enum http_server_wifi_connect_status
-{
-	NONE = 0,
-	HTTP_WIFI_STATUS_CONNECTING,
-	HTTP_WIFI_STATUS_CONNECT_FAILED,
-	HTTP_WIFI_STATUS_CONNECT_SUCCESS,
-	HTTP_WIFI_STATUS_DISCONNECTED,
-} http_server_wifi_connect_status_e;
+#define OTA_UPDATE_PENDING 		0
+#define OTA_UPDATE_SUCCESSFUL	1
+#define OTA_UPDATE_FAILED		-1
 
 /**
  * Messages for the HTTP monitor
@@ -28,10 +13,8 @@ typedef enum http_server_message
 	HTTP_MSG_WIFI_CONNECT_INIT = 0,
 	HTTP_MSG_WIFI_CONNECT_SUCCESS,
 	HTTP_MSG_WIFI_CONNECT_FAIL,
-	HTTP_MSG_WIFI_USER_DISCONNECT,
 	HTTP_MSG_OTA_UPDATE_SUCCESSFUL,
 	HTTP_MSG_OTA_UPDATE_FAILED,
-	HTTP_MSG_TIME_SERVICE_INITIALIZED,
 } http_server_message_e;
 
 /**
@@ -59,5 +42,12 @@ void http_server_start(void);
  * Stops the HTTP server.
  */
 void http_server_stop(void);
+
+/**
+ * Timer callback function which calls esp_restart upon successful firmware update.
+ */
+void http_server_fw_update_reset_callback(void *arg);
+
+
 
 #endif /* MAIN_HTTP_SERVER_H_ */
